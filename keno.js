b4ca9538e0be5939cfff0b83bb871a90399b1cf0
hollieherbel <herbel6521@gmail.com>
2020-06-06 21:28:15 -0500

    added random number picker

--- keno.js
+++ keno.js
@@ -1,4 +1,18 @@
 const welcome = prompt("Welcome to the Keno game! Are you ready to play?")
   if (welcome == 'yes') {
-    alert('Great!  Please choose 10 numbers from 1 to 80.')
+    alert('Great!')
+  }
+
+const pickNums = prompt("You can win up to $200.  Please choose one number from 1 to 15.")
+  if (pickNums) {
+    alert(`You chose ${pickNums}.`)
+  }
+
+const winningNum = Math.floor(Math.random() * 10 + 5)
+
+const matchNum = prompt(`The winning number is ${winningNum}.`)
+  if (pickNums == winningNum) {
+    alert('Congrats, you won $20! Pick another number.')
+  } else if (pickNums != winningNum) {
+    alert("I'm sorry, you did not win this round. Choose another number.")
   }
