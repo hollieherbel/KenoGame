0b25250d3a5a91db27ef645184a5a4fbaa855ccc
hollieherbel <herbel6521@gmail.com>
2020-06-07 23:49:17 -0500

    added wallet and total earnings

--- keno.js
+++ keno.js
@@ -1,18 +1,26 @@
+const wallet = 50
+
 const welcome = prompt("Welcome to the Keno game! Are you ready to play?")
   if (welcome == 'yes') {
     alert('Great!')
-  }
+  } 
 
-const pickNums = prompt("You can win up to $200.  Please choose one number from 1 to 15.")
+const pickNums = prompt("You will start with $50, but could win up to $200! Please choose one number from 1 to 15.")
   if (pickNums) {
     alert(`You chose ${pickNums}.`)
   }
 
-const winningNum = Math.floor(Math.random() * 10 + 5)
+let winningNum = Math.floor(Math.random() * 10 + 5)
 
-const matchNum = prompt(`The winning number is ${winningNum}.`)
-  if (pickNums == winningNum) {
-    alert('Congrats, you won $20! Pick another number.')
-  } else if (pickNums != winningNum) {
-    alert("I'm sorry, you did not win this round. Choose another number.")
-  }
+
+if (pickNums == winningNum) {
+  alert(`The winning number is ${winningNum}. Congrats, you won $50! Pick another number.`)
+} else if (pickNums != winningNum) {
+  alert(`I'm sorry, the winning number is ${winningNum}. You did not win this round. Choose another number.`)
+} 
+
+const earnings = wallet + 50
+
+if (pickNums == winningNum) {
+  alert(`You now have $${earnings}`)
+}
