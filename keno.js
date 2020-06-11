1233601ef3b8aa8941c3e9c866e46e78fd0ef363
hollieherbel <herbel6521@gmail.com>
2020-06-11 09:46:21 -0500

    added a do while loop

--- keno.js
+++ keno.js
@@ -1,9 +1,13 @@
 const wallet = 50
 
-const betNum = prompt("Welcome to the Keno game! You will start with $50, but could win up to $200! How much would you like to bet?")
-  if (betNum) {
-    alert(`Good luck!`)
-  } 
+const kenoWelcome = prompt("Welcome to the Keno game! You will start with $50! Are you ready to play?")
+  if (kenoWelcome == 'yes' | 'Yes') {
+    alert('Great! Good Luck!')
+  }
+  
+do {
+
+const betNum = prompt("How much would you like to bet?")
 
 const pickNums = prompt("Please choose a number between 1 and 15.")
   if (pickNums) {
@@ -12,12 +16,12 @@ const pickNums = prompt("Please choose a number between 1 and 15.")
 
 let winningNum = Math.floor(Math.random() * 10 + 5)
 
-
 if (pickNums == winningNum) {
   alert(`The winning number is ${winningNum}. Congrats, you won!`)
 } else if (pickNums != winningNum) {
   alert(`I'm sorry, the winning number is ${winningNum}. You did not win this round.`) 
 }
+
 const moreEarnings = Number(wallet) + Number(betNum)
 const lostEarnings = wallet - betNum
 
@@ -29,13 +33,19 @@ if (pickNums != winningNum) {
   alert(`You now have $${lostEarnings}.`)
 }
 
-
-
-const cashOut = prompt("Want to play again?")
-  if (cashOut == 'yes' | 'Yes')  {
-    alert('Great! Please pick another number.')
-  } else if (cashOut != 'yes' | 'Yes' && pickNums == winningNum) {
+const keepPlaying = prompt("Want to play again?")
+  if (keepPlaying == 'yes' | 'Yes')  {
+    alert('Great!')
+  } 
+  
+  if (keepPlaying != 'yes' | 'Yes' && pickNums == winningNum) {
     alert(`Thanks for playing. You will go home with $${moreEarnings}.`)
-  } else if (cashOut != 'yes' | 'Yes' && pickNums != winningNum) {
+  } else if (keepPlaying != 'yes' | 'Yes' && pickNums != winningNum) {
     alert(`Thanks for playing. You will go home with $${lostEarnings}.`)
+  } 
+
+  if (keepPlaying == 'no' | 'No') {
+    break;
   }
+
+} while (true);
